3fb36d94aa80f06118efc4d0990e938041ac6be1
SaneKCLOFF <sanekcl.sanek@gmail.com>
2023-03-15 15:22:00 +0300

    New Project

---
+++ Olympiada/TestApp/TestApp/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Entities
+{
+    internal class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public UserProfile UserProfile { get; set; } = null!;
+    }
+}
---
+++ Olympiada/TestApp/TestApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Entities;
+
+namespace TestApp.Models
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        private const string connectionString = @"Server=localhost\SQLEXPRESS; Database=TestAppDataBase; Trusted_Connection=true; TrustServerCertificate=true";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<UserProfile> UserProfiles { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserProfile>()
+                .HasOne(u => u.User)
+                .WithOne(up => up.UserProfile)
+                .HasForeignKey<UserProfile>
+                (u => u.UserId);
+        }
+    }
+}
